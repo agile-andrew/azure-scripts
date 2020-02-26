7ee1dd3c960d116f6514408b9d44f2c621718a6d
Andrew Penn <61515084+agile-andrew@users.noreply.github.com>
2020-02-26 11:11:15 -0800

    Update description & add disclaimer

--- set-tags-on-all-vms-with-ahub-enabled.ps1
+++ set-tags-on-all-vms-with-ahub-enabled.ps1
@@ -1,13 +1,37 @@
-#################################################
-# filename: "set-tags-on-all-vms-with-ahub-enabled.ps1"
-# author: Andrew Penn
-#
-# Adds a tag to all VMs in the current scope with Azure Hybrid Use Benefit (AHUB) enabled.
-# This tag can then be queried for in Cost Analysis to track costs of only AHUB-enabled VMs.
-# At the time of this writing, Cost Analysis does not have native feature to filter VMs by AHUB status.
-#
-# NOTE: New Tags can take ~1 day to reflect in Cost Analysis before being queryable
-#################################################
+<#
+.SYNOPSIS
+    Author:     Andrew Penn, MSFT
+    Version:    1.0.0
+    Created:    26/2/2020
+    Updated:    26/2/2020
+
+.DESCRIPTION
+    This script automates the process of applying a tag to all Virtual Machines in the current scope
+    with Azure Hybrid Use Benefit (AHUB) enabled.
+    
+    These tags can then be queried for in Cost Analysis to track costs of only AHUB-enabled VMs.
+    At the time of this writing, Cost Analysis does not have native capability to filter VMs by AHUB status.
+    
+    Note that new Tags can take 1 day to reflect in Cost Analysis before being queryable.
+
+.NOTES
+    THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED 
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR 
+    FITNESS FOR A PARTICULAR PURPOSE.
+
+    This sample is not supported under any Microsoft standard support program or service. 
+    The script is provided AS IS without warranty of any kind. Microsoft further disclaims all
+    implied warranties including, without limitation, any implied warranties of merchantability
+    or of fitness for a particular purpose. The entire risk arising out of the use or performance
+    of the sample and documentation remains with you. In no event shall Microsoft, its authors,
+    or anyone else involved in the creation, production, or delivery of the script be liable for 
+    any damages whatsoever (including, without limitation, damages for loss of business profits, 
+    business interruption, loss of business information, or other pecuniary loss) arising out of 
+    the use of or inability to use the sample or documentation, even if Microsoft has been advised 
+    of the possibility of such damages, rising out of the use of or inability to use the sample script, 
+    even if Microsoft has been advised of the possibility of such damages. 
+
+#>
 
 # Get all VMs in current scope
 $allVms = Get-AzVm 
